588f44fcb8b66227b30802721d61e26efc0eff96
Hannes Gräuler <hgraeule@uos.de>
2015-06-24 19:33:15 +0200

    Code cleanup

--- django_moment/urls.py
+++ django_moment/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include, patterns, url
 from django.views.generic import TemplateView
 
-from qrmecore.models import Box
-
 urlpatterns = patterns('django_moment.views',
     url(r'^event/(?P<event_name>[\.a-zA-Z0-9_]+)/(?P<obj_name>[\.a-zA-Z0-9_]+)/period.json', 'event_period_json'),
     url(r'^event/(?P<event_name>[\.a-zA-Z0-9_]+)/(?P<obj_name>[\.a-zA-Z0-9_]+)/', 'event_dashboard'),
--- django_moment/views.py
+++ django_moment/views.py
@@ -8,12 +8,7 @@ from moment import (COUNTER_ALIASES, EVENT_ALIASES, DayCounter, MonthCounter,
 
 from .models import Counter, Event
 
-dashboard_counter = Counter('dashboard')
-dashboard_update_counter = Counter('dashboard_update')
-dashboard_event = Event('dashboardev', 'languages')
-
 def counter_period_json(request, counter_name):
-    dashboard_update_counter.inc()
     response_dict = {}
     for (key, cls) in COUNTER_ALIASES.iteritems():
         counter = cls.from_date(counter_name)
@@ -27,29 +22,25 @@ def counter_period_json(request, counter_name):
     return JsonResponse(response_dict)
 
 def event_period_json(request, event_name, obj_name):
-    dashboard_event.record(request.META['SERVER_NAME'])
     response_dict = {}
     for (key, cls) in EVENT_ALIASES.iteritems():
-        event = cls.from_date(event_name, sequence='languages')
+        event = cls.from_date(event_name, sequence='users')
         items = []
         for i in range(10):
             items.append(dict({obj_name: event.is_recorded(obj_name)},
                             period_start=event.period_start(),
                             period_end=event.period_end()))
             event = event.delta(-1)
-            event.sequence = 'languages'
+            event.sequence = 'users'
         response_dict[key] = items
     return JsonResponse(response_dict)
 
 def counter_dashboard(request, counter_name):
-    dashboard_counter.inc()
     return render(request, 'django_moment/counter_dashboard.html', {
         'counter': counter_name
     })
 
 def event_dashboard(request, event_name, obj_name):
-    dashboard_event.record(request.META['SERVER_NAME'])
-    print request.META['SERVER_NAME']
     period_json_url = reverse(event_period_json, kwargs=dict(event_name=event_name, obj_name=obj_name))
     return render(request, 'django_moment/event_dashboard.html', {
         'event': event_name,
