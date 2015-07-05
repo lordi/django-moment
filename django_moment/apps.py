6b2db1a36b0cf1f5a2aa294d1052b6cbaad55d76
Hannes Gräuler <hgraeule@uos.de>
2015-07-05 17:24:21 +0200

    Implement a way to configure redis connection

--- django_moment/apps.py
+++ django_moment/apps.py
@@ -1,15 +1,22 @@
 from django.apps import AppConfig
+from django.conf import settings
+
 from moment import conf
+
 
 class MomentConfig(AppConfig):
     name = 'django_moment'
     verbose_name = "Moment"
 
+    defaults = {
+        'DJANGO_MOMENT_REDIS_HOST': 'localhost',
+        'DJANGO_MOMENT_REDIS_PORT': 6379
+    }
+
+    def _get_setting(self, setting):
+        return getattr(settings, setting, self.defaults.get(setting))
+
     def ready(self):
-        # Register default connection 
-        conf.register_connection()
-
-        # Register some other connection
-        conf.register_connection(alias='analytics', host='localhost', port=6379)
-
-        self.conn = conf.get_connection('analytics')
+        conf.register_connection(alias='default',
+                host=self._get_setting('DJANGO_MOMENT_REDIS_HOST'),
+                port=self._get_setting('DJANGO_MOMENT_REDIS_PORT'))
